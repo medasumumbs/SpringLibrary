924a4df628812944740e8ea750776ee4cea10617
medasumumbs <semen.muravin@gmail.com>
2024-07-06 16:42:31 +0300

    Added russian localization (utf8 encoding for spring)

--- src/main/java/ru/muravin/springLibrary/config/MySpringMvcDispatcherSerlvetIntitializer.java
+++ src/main/java/ru/muravin/springLibrary/config/MySpringMvcDispatcherSerlvetIntitializer.java
@@ -2,7 +2,10 @@ package ru.muravin.springLibrary.config;
 
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
-
+import org.springframework.web.filter.CharacterEncodingFilter;
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import java.util.EnumSet;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
@@ -28,7 +31,19 @@ public class MySpringMvcDispatcherSerlvetIntitializer extends AbstractAnnotation
     @Override
     public void onStartup(ServletContext aServletContext) throws ServletException {
         super.onStartup(aServletContext);
+        registerCharacterEncodingFilter(aServletContext);
         registerHiddenFieldFilter(aServletContext);
+    }
+
+    private void registerCharacterEncodingFilter(ServletContext aContext) {
+        EnumSet<DispatcherType> dispatcherTypes = EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD);
+
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+
+        FilterRegistration.Dynamic characterEncoding = aContext.addFilter("characterEncoding", characterEncodingFilter);
+        characterEncoding.addMappingForUrlPatterns(dispatcherTypes, true, "/*");
     }
 
     private void registerHiddenFieldFilter(ServletContext aContext) {
